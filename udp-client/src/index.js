37aca8d65563dc13955a5bb0cd44792c340054ec
Bartlomiej Kotarski <bkotarski1@gmail.com>
2021-04-29 22:54:29 +0200

    opt

--- udp-client/src/index.js
+++ udp-client/src/index.js
@@ -1,61 +1,41 @@
 const dgram = require('dgram');
 
+// types
+const ActionType = require('./types');
+
 const PORT = 8080;
 const HOST = '127.0.0.1';
 
 const socket = dgram.createSocket('udp4');
 
-const createFile = (fileName) =>
-    new Promise((resolve, reject) =>
-        socket.send(
-            Buffer.from(JSON.stringify({ action: 'CREATE_FILE', fileName })),
-            PORT,
-            HOST,
-            (err) => {
-                if (err) reject(err);
-                resolve('1. File should be created');
-            },
-        ),
-    );
-
-const writeToFile = (fileName, msg) =>
-    new Promise((resolve, reject) =>
-        socket.send(
-            Buffer.from(
-                JSON.stringify({
-                    action: 'WRITE_INTO_FILE',
-                    fileName,
-                    content: msg,
-                }),
-            ),
-            PORT,
-            HOST,
-            (err) => {
-                if (err) reject(err);
-                resolve('2. Content should be saved');
-            },
-        ),
-    );
-
-const deleteFile = (fileName) =>
-    new Promise((resolve, reject) =>
-        socket.send(
-            Buffer.from(JSON.stringify({ action: 'DELETE_FILE', fileName })),
-            PORT,
-            HOST,
-            (err) => {
-                if (err) reject(err);
-                resolve('3. File should be removed');
-            },
-        ),
-    );
+const sendMsgBySocket = (
+    command, // { action: ActionType, fileName: string, content?: string }
+) =>
+    new Promise((resolve, reject) => {
+        const buffer = Buffer.from(JSON.stringify(command));
+        socket.send(buffer, PORT, HOST, (err) => {
+            if (err) reject(err);
+            resolve(command.action);
+        });
+    });
 
 const startApp = async () => {
-    console.log(await createFile('test.txt'));
-    console.log(await writeToFile('test.txt', 'I want to save the message'));
+    /**
+     * Create new file
+     */
+    console.log(await sendMsgBySocket({ action: ActionType.createFile, fileName: 'test.txt' }));
+    /**
+     * Write msg to created file
+     */
+    console.log(
+        await sendMsgBySocket({ action: ActionType.writeIntoFile, fileName: 'test.txt', content: 'Ala ma kota' }),
+    );
 
+    /**
+     * Delete file
+     */
     setTimeout(async () => {
-        console.log(await deleteFile('test.txt'));
+        console.log(await sendMsgBySocket({ action: ActionType.deleteFile, fileName: 'test.txt' }));
         socket.close();
     }, 10000);
 };
